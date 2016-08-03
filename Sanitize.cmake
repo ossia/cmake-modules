45672a418b05c6b684e3ef030b17c59e77ed68d8
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2016-08-03 18:04:39 +0200

    correct link with gcc

--- Sanitize.cmake
+++ Sanitize.cmake
@@ -1,22 +1,22 @@
 function(sanitize_clang theTarget)
-	target_compile_options(${theTarget} PUBLIC
-		$<$<CONFIG:Debug>:-fsanitize=undefined>
-		$<$<CONFIG:Debug>:-fsanitize=integer>
-		$<$<CONFIG:Debug>:-fsanitize=address>
-		$<$<CONFIG:Debug>:-fno-omit-frame-pointer>
-		)
-	target_link_libraries(${theTarget} PUBLIC
-		$<$<CONFIG:Debug>:-fsanitize=address>
-		)
+    target_compile_options(${theTarget} PUBLIC
+        $<$<CONFIG:Debug>:-fsanitize=undefined>
+        $<$<CONFIG:Debug>:-fsanitize=integer>
+        $<$<CONFIG:Debug>:-fsanitize=address>
+        $<$<CONFIG:Debug>:-fno-omit-frame-pointer>
+        )
+    target_link_libraries(${theTarget} PUBLIC
+        $<$<CONFIG:Debug>:-fsanitize=address>
+        )
 endfunction()
 
 function(sanitize_appleclang theTarget)
 target_compile_options(${theTarget} PUBLIC
-	$<$<CONFIG:Debug>:-fsanitize=address>
-	$<$<CONFIG:Debug>:-fno-omit-frame-pointer>
+    $<$<CONFIG:Debug>:-fsanitize=address>
+    $<$<CONFIG:Debug>:-fno-omit-frame-pointer>
   )
 target_link_libraries(${theTarget} PUBLIC
-	$<$<CONFIG:Debug>:-fsanitize=address>
+    $<$<CONFIG:Debug>:-fsanitize=address>
   )
 endfunction()
 
@@ -28,8 +28,8 @@ function(sanitize_gcc theTarget)
         $<$<CONFIG:Debug>:-fno-omit-frame-pointer>
       )
       target_link_libraries(${theTarget} PUBLIC
-          $<$<CONFIG:Debug>:-lubsan>
-          $<$<CONFIG:Debug>:-lasan>
+          $<$<CONFIG:Debug>:-fsanitize=address>
+          $<$<CONFIG:Debug>:-fsanitize=undefined>
       )
     endif()
 endfunction()
@@ -39,13 +39,13 @@ endfunction()
 
 function(sanitize_build theTarget)
     if ("${CMAKE_CXX_COMPILER_ID}" MATCHES "AppleClang")
-		sanitize_appleclang(${theTarget})
+        sanitize_appleclang(${theTarget})
     elseif ("${CMAKE_CXX_COMPILER_ID}" MATCHES "Clang")
-		if(APPLE)
-			sanitize_appleclang(${theTarget})
-		else()
-			sanitize_clang(${theTarget})
-		endif()
+        if(APPLE)
+            sanitize_appleclang(${theTarget})
+        else()
+            sanitize_clang(${theTarget})
+        endif()
     elseif ("${CMAKE_CXX_COMPILER_ID}" MATCHES "MSVC")
         sanitize_msvc(${theTarget})
     elseif ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")
