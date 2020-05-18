b931c2a91a895daec4bf75ce18a5ccc110231b4e
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2020-05-19 01:21:11 +0200

    add a module to handle static qt plugins

---
+++ StaticQt.cmake
@@ -0,0 +1,37 @@
+function(disable_qt_plugins theTarget)
+  if(NOT TARGET Qt5::Core)
+    return()
+  endif()
+
+  get_target_property(QtType Qt5::Core TYPE)
+
+  if(NOT ("${QtType}" STREQUAL "STATIC_LIBRARY"))
+    return()
+  endif()
+
+  set_target_properties(${theTarget}
+    PROPERTIES
+      QT_NO_PLUGINS
+     "${Qt5Core_PLUGINS};${Qt5Gui_PLUGINS};${Qt5Widgets_PLUGINS};${Qt5Network_PLUGINS};${Qt5Qml_PLUGINS}"
+  )
+endfunction()
+
+function(enable_minimal_qt_plugins theTarget)
+  if(NOT TARGET Qt5::Core)
+    return()
+  endif()
+
+  get_target_property(QtType Qt5::Core TYPE)
+  if(NOT ("${QtType}" STREQUAL "STATIC"))
+    return()
+  endif()
+
+  if(TARGET Qt5::QXcbIntegrationPlugin)
+    target_link_libraries(${theTarget} PRIVATE Qt5::QXcbIntegrationPlugin Qt5::QXcbGlxIntegrationPlugin)
+  elseif(TARGET Qt5::QWindowIntegrationPlugin)
+    target_link_libraries(${theTarget} PRIVATE  Qt5::QWindowIntegrationPlugin)
+  elseif(TARGET Qt5::QCocoaIntegrationPlugin)
+    target_link_libraries(${theTarget} PRIVATE  Qt5::QCocoaIntegrationPlugin)
+  endif()
+
+endfunction()
